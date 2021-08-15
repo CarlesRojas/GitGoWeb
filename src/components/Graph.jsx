a344b4cb671d41eb4c6e17fb53cd9a58d904f8dd
Carles <carlesrojas@outlook.com>
2021-08-15 20:29:49 +0200

    Draw Branch V3

--- src/components/Graph.jsx
+++ src/components/Graph.jsx
@@ -25,58 +25,134 @@ export default function Graph() {
         // Branches
         var branches = [new Array(sortedCommits.length).fill(false)];
 
-        // Find parent I index
-        const findParent = (hash) => {
+        // Find commit I index
+        const findCommitIndex = (hash) => {
             for (let i = 0; i < sortedCommits.length; i++) if (sortedCommits[i].commit.long === hash) return i;
             return -1;
         };
 
-        // Place branch
-        // const placeBranch = (minI, maxI, branch, i) => {
-        //     while (branch >= branches.length) branches.push(new Array(sortedCommits.length).fill(false));
+        // Branches commits
+        var branchesCommits = [];
 
-        //     if (i > maxI) return true;
+        // Commits that have not loaded parents (BranchIndex -> CommitHash)
+        var lastCommit = {};
 
-        //     if (!branches[branch][i]) {
-        //         if (placeBranch(minI, maxI, branch, i + 1)) {
-        //             branches[branch][i] = true;
-        //             return true;
-        //         } else return false;
-        //     } else return false;
-        // };
+        // Find Start and Finish of a branch
+        const exploreBranch = (currCommitI, minI, branchI) => {
+            // Check if commit has been explored
+            const isExplored = (commitI) => {
+                return "explored" in sortedCommits[commitI] && sortedCommits[commitI].explored;
+            };
 
-        // // Find Start and Finish of a branch
-        // const getMaxI = (currCommitI, minI) => {
-        //     // Set commit as explored
-        //     sortedCommits[currCommitI].explored = true;
+            // Set commit as explored
+            sortedCommits[currCommitI].explored = true;
 
-        //     // No Parent
-        //     if (!("parent" in sortedCommits[currCommitI]) || sortedCommits[currCommitI].parent.length <= 0) {
-        //     }
+            // Add commit to its branch
+            branchesCommits[branchI].commits.push(sortedCommits[currCommitI].commit.long);
+            console.log(sortedCommits[currCommitI].commit.long);
 
-        //     // One Parent
-        //     else if (sortedCommits[currCommitI].parent.length === 1) {
-        //     }
+            // No Parent -> Add commit to its branch and exit
+            if (!("parent" in sortedCommits[currCommitI]) || sortedCommits[currCommitI].parent.length <= 0) {
+                branchesCommits[branchI].max = currCommitI;
+                console.log("End");
+                return;
+            }
 
-        //     // Two Parents
-        //     else {
-        //     }
+            // One Parent
+            else if (sortedCommits[currCommitI].parent.length === 1) {
+                // Parent commit
+                var parentCommitI = findCommitIndex(sortedCommits[currCommitI].parent[0]);
 
-        //     for (let i = 0; i < sortedCommits[currCommitI].parent.length; i++) {
-        //         const parentI = findParent(sortedCommits[currCommitI].parent[i]);
+                // If parent is not loaded
+                if (parentCommitI < 0) {
+                    branchesCommits[branchI].max = currCommitI;
+                    lastCommit[branchI] = sortedCommits[currCommitI].parent[0];
+                    console.log("Parent Not Loaded");
+                }
+                // If parent is not already explored -> Keep exploring
+                else if (!isExplored(parentCommitI)) exploreBranch(parentCommitI, minI, branchI);
+                // Otherwise -> End of branch
+                else {
+                    branchesCommits[branchI].max = parentCommitI;
+                    console.log("End Branch");
+                }
+            }
 
-        //         // Parent not loaded
-        //         if (parentI < 0) return storedCommits.le;
+            // Two Parents
+            else {
+                // First parent commit
+                var firstParentCommitI = findCommitIndex(sortedCommits[currCommitI].parent[0]);
 
-        //         // Parent already explored
-        //         if ("explored" in sortedCommits[parentI]) return getMaxI(parentI, minI);
-        //     }
-        // };
+                // If parent is not loaded
+                if (firstParentCommitI < 0) {
+                    branchesCommits[branchI].max = currCommitI;
+                    lastCommit[branchI] = sortedCommits[currCommitI].parent[0];
+                    console.log("Parent Not Loaded");
+                }
+                // If first parent is not already explored -> Keep exploring
+                else if (!isExplored(firstParentCommitI)) exploreBranch(firstParentCommitI, minI, branchI);
+                // Otherwise -> End of branch
+                else {
+                    branchesCommits[branchI].max = firstParentCommitI;
+                    console.log("End Branch");
+                }
 
-        // // Get Graph
-        // getMaxI(0, 0);
+                // Second parent commit
+                var secondParentCommitI = findCommitIndex(sortedCommits[currCommitI].parent[1]);
 
-        // console.log(sortedCommits);
+                // If parent is not loaded
+                if (secondParentCommitI < 0) {
+                    branchesCommits[branchI].max = currCommitI;
+                    lastCommit[branchesCommits.length - 1] = sortedCommits[currCommitI].parent[1];
+                    console.log("Parent Not Loaded");
+                }
+                // If second parent is not already explored -> Start new branch with the second parent
+                else if (!isExplored(secondParentCommitI)) {
+                    console.log("Start Branch");
+                    branchesCommits.push({ commits: [], min: currCommitI, max: currCommitI });
+                    exploreBranch(secondParentCommitI, secondParentCommitI, branchesCommits.length - 1);
+                }
+                // Otherwise -> End of branch
+                else {
+                    branchesCommits[branchI].max = secondParentCommitI;
+                    console.log("End Branch");
+                }
+            }
+        };
+
+        // Try to place branch that spans from 'minI' to 'maxI' in the 'branch' column
+        const placeBranch = (minI, maxI, branch, i) => {
+            while (branch >= branches.length) branches.push(new Array(sortedCommits.length).fill(false));
+
+            if (i > maxI) return true;
+
+            if (!branches[branch][i]) {
+                if (placeBranch(minI, maxI, branch, i + 1)) {
+                    branches[branch][i] = true;
+                    return true;
+                } else return false;
+            } else return false;
+        };
+
+        // Get Branches
+        for (let i = 0; i < sortedCommits.length; i++) {
+            if (!("explored" in sortedCommits[i]) || !sortedCommits[i].explored) {
+                branchesCommits.push({ commits: [], min: i, max: i });
+                exploreBranch(i, i, branchesCommits.length - 1);
+            }
+        }
+
+        // ROJAS
+        // Create object with hashes as keys and the commits as values
+        // Set children in each commit
+        // After exploreBranch() find min and max with the farthest parent and the farthest children
+
+        // Place branches
+        // for (let i = 0; i < branchesCommits.length; i++) {
+        //     const element = branchesCommits[i];
+        // }
+
+        console.log(branchesCommits);
 
         return [];
     };
@@ -139,7 +215,6 @@ export default function Graph() {
     const tree = hidden.tree ? null : <Tree commits={commits} />;
     const messages = hidden.messages ? null : <Messages commits={commits} />;
 
-    console.log(commits);
     return (
         <div className="graph">
             <div className="commits">
--- src/components/Tree.jsx
+++ src/components/Tree.jsx
@@ -2,27 +2,6 @@ import React, { useEffect } from "react";
 
 export default function Tree({ commits }) {
     // #################################################
-    //   SORTING COMMITS
-    // #################################################
-
-    // On component mount
-    useEffect(() => {
-        console.log(commits);
-
-        const getGitGraph = (sortedCommits) => {
-            // Branches
-            var branches = [new Array(sortedCommits.length).fill(false)];
-
-            // Find Start and Finish of a branch
-            const findMinAndMaxHeightOfBranch = (initialCommit, minIndex) => {};
-
-            // GetGraph
-        };
-
-        getGitGraph(commits);
-    }, [commits]);
-
-    // #################################################
     //   COMPONENT MOUNT
     // #################################################
 
